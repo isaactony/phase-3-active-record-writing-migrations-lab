ff53b394527b9e225ce6bba30f231b60ec76dd42
isaac <isaactonyloi@gmail.com>
2023-03-06 08:30:03 +0300

    active records

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,4 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[6.1]
   def change
+    create_table :students do |t|
+      t.string :name
+      # the id column is gemerated automatically for every table! no need to specify it here.
+    end
   end
 end
